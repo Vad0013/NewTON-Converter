bbb672d42bf75ef0db1ff0b205b52f6a3ce5ebcf
VadDiCatliff <vadim15311@gmail.com>
2023-08-21 20:36:44 +0300

    Added console interface

--- Program.cs
+++ Program.cs
@@ -2,29 +2,65 @@
 using Newtonsoft.Json.Linq;
 using NewTONConverter;
 
+Console.WriteLine(" NewRTON Converter");
+Console.WriteLine(" Author: VadDiCatliff");
+Console.WriteLine(" YT: https://www.youtube.com/@Vad0013");
+Console.WriteLine(" Version: 1.0.0");
+Console.WriteLine(" <--------------------------------------------->");
 
-/*Console.Write("Manifest.newton: ");
-string? filepath = Console.ReadLine();
-
-JObject jobject = NewtonReader.ConvertToJObject(filepath);
-
-using (FileStream fileStream = File.Open(filepath + ".json", FileMode.Create))
+while (true)
 {
-    using (StreamWriter streamWriter = new StreamWriter(fileStream))
+    try
     {
-        using (JsonTextWriter jsonWriter = new JsonTextWriter(streamWriter))
-        {
-            jsonWriter.Formatting = Formatting.Indented;
-            jsonWriter.IndentChar = '\t';
-            jsonWriter.Indentation = 1;
+        Console.Write(" Mode(0 - Encode, 1 - Decode, 2 - Exit): ");
+        int mode = Convert.ToInt32(Console.ReadLine());
 
-            jobject.WriteTo(jsonWriter);
+        if (mode == 0)
+        {
+            Console.Write(" JSON (Manifest): ");
+            string jsonPath = Console.ReadLine().Replace("\"", "");
+
+            JObject jobject = JObject.Parse(File.ReadAllText(jsonPath));
+            NewtonWriter.ConvertFromJObject(jobject, jsonPath + ".newton");
+
+            Console.WriteLine(" COMPLETED!");
+        }
+        else if (mode == 1)
+        {
+            Console.Write(" NewTON: ");
+            string newtonPath = Console.ReadLine().Replace("\"", "");
+            JObject jobject = NewtonReader.ConvertToJObject(newtonPath);
+
+            using (FileStream fileStream = File.Open(newtonPath + ".json", FileMode.Create))
+            {
+                using (StreamWriter streamWriter = new StreamWriter(fileStream))
+                {
+                    using (JsonTextWriter jsonWriter = new JsonTextWriter(streamWriter))
+                    {
+                        jsonWriter.Formatting = Formatting.Indented;
+                        jsonWriter.IndentChar = '\t';
+                        jsonWriter.Indentation = 1;
+
+                        jobject.WriteTo(jsonWriter);
+                    }
+                }
+            }
+
+            Console.WriteLine(" COMPLETED!");
+        }
+        else
+        {
+            break;
         }
     }
-}*/
+    catch (Exception ex)
+    {
+        Console.WriteLine(" !ERROR: " + ex.Message);
+    }
 
-Console.Write("Manifest.json: ");
-string? filepath = Console.ReadLine();
+    Console.WriteLine(" <--------------------------------------------->");
+}
 
-JObject jobject = JObject.Parse(File.ReadAllText(filepath));
-NewtonWriter.ConvertFromJObject(jobject, filepath + ".newton");
+Console.Write(" Press any key to continue...");
+Console.ReadKey();
+
